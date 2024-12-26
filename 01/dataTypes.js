2f88b62907608b82b1d6bc6dc34e92ebe628fce4
priyanshuKshk <kshkpriyanshu64@gmail.com>
2024-12-26 14:48:36 +0000

    dataTypes.js

---
+++ 01/dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all thhe js code in newer version
+//alert(3+3); // we are using node not the browser 
+
+/*number => 2 to the power 53
+bigint=>
+string =""
+boolean = true/false
+null=standalone value
+undefined =
+symbol
+*/
+console.log(typeof null)//obkect
+console.log(typeof undefined)//undefined
---
+++ 01/variables.js
@@ -0,0 +1,7 @@
+const accountId = "12333";
+var accountEmail = "kaushikpriyanshu64@gmail.com"
+accountName = "priyanshu kaushik"
+accountPlace = "jaipur";
+accountPassword =  "1234"
+console.table([accountEmail ,accountId , accountName , accountPlace])
+// prefer not to use var beacuse of issue in functional scope;
