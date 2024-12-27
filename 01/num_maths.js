16c9f1c92dece49f1c753390bb09044eeea74ae7
priyanshuKshk <kshkpriyanshu64@gmail.com>
2024-12-27 07:02:58 +0000

    num_maths.js

---
+++ 01/num_maths.js
@@ -0,0 +1,27 @@
+// const num = 102;
+// console.log(num)
+// const num1= new Number(678)
+// console.log(num1);
+// console.log(num1.toString().length);
+// console.log(num1.toFixed(2));
+// const num2 = 123.323
+
+// console.log(num2.toPrecision(3));//123
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));//1,00,000
+
+//++++++++++++++++++++++Maths++++++++++++++++++++++++++
+// console.log("MATHS");
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(9.8));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1);//to avoid zero
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random()*(max - min +1))+min);
+
+
+ 
