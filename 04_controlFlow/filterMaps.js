ac7b9b900117e5c2e5f3b9cee9599590f417a115
priyanshuKshk <kshkpriyanshu64@gmail.com>
2024-12-29 06:55:08 +0000

    filterMaps

---
+++ 04_controlFlow/filterMaps.js
@@ -0,0 +1,11 @@
+// for each do not return any value so we use filters
+const myNums = [1,2,3,4,5,6,7,8,9]
+// const newNums = myNums.filter((num) =>num >4)
+// console.log(newNums)
+const newNums =[]
+myNums.forEach((num) =>{
+    if(num >4){
+        newNums.push(num)
+    }
+})
+console.log(newNums)
